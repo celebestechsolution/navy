41b5789a2fa97019fc24a924e03b59d21069099f
Mario <mariomad2296@gmail.com>
2024-10-09 20:52:35 +0800

    Add all component from Shadcn

--- src/lib/plugins/shadcn-plugin.ts
+++ src/lib/plugins/shadcn-plugin.ts
@@ -66,6 +66,10 @@ export const shadcnPlugin = plugin(
     {
         theme: {
             extend: {
+                animation: {
+                    'accordion-down': 'accordion-down 0.2s ease-out',
+                    'accordion-up': 'accordion-up 0.2s ease-out',
+                },
                 borderRadius: {
                     lg: 'var(--radius)',
                     md: 'calc(var(--radius) - 2px)',
@@ -115,6 +119,24 @@ export const shadcnPlugin = plugin(
                 },
                 fontFamily: {
                     sans: ['Figtree', ...fontFamily.sans],
+                },
+                keyframes: {
+                    'accordion-down': {
+                        from: {
+                            height: '0',
+                        },
+                        to: {
+                            height: 'var(--radix-accordion-content-height)',
+                        },
+                    },
+                    'accordion-up': {
+                        from: {
+                            height: 'var(--radix-accordion-content-height)',
+                        },
+                        to: {
+                            height: '0',
+                        },
+                    },
                 },
             },
         },
