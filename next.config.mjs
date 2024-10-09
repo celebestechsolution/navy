b627aa74e4727ef391081ac6939b732830451724
Mario <mariomad2296@gmail.com>
2024-10-09 20:28:47 +0800

    Update folder structure on pages directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+        styledComponents: true,
+    },
+    async redirects() {
+        return [
+            // General
+            {
+                destination: '/',
+                source: '/general/home',
+                permanent: true,
+            },
+            // Guest
+            {
+                destination: '/login',
+                source: '/guest/login',
+                permanent: true,
+            },
+            // Auth
+        ];
+    },
+    async rewrites() {
+        return [
+            // General
+            {
+                source: '/',
+                destination: '/general/home',
+            },
+            // Guest
+            {
+                source: '/login',
+                destination: '/guest/login',
+            },
+            // Auth
+        ];
+    },
 };
 
 export default nextConfig;
