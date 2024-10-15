8633310bbc56c4c79adf71244540591d02d00a6d
Mario <mariomad2296@gmail.com>
2024-10-16 02:38:47 +0800

    Update Sheet

--- src/layouts/mobile-sheet.tsx
+++ src/layouts/mobile-sheet.tsx
@@ -109,7 +109,7 @@ const MobileSheet = () => {
                     <TablerIcon name='IconMenuDeep' className='size-6 stroke-2 text-primary' />
                 </Button>
             </SheetTrigger>
-            <SheetContent side='left' className='m-2 flex h-dvh w-4/5 flex-col rounded-md border p-0'>
+            <SheetContent side='left' className='flex w-4/5 flex-col p-0'>
                 <SheetHeader className='space-y-0 border-b p-4'>
                     <SheetTitle className='sr-only'>Sheet Title</SheetTitle>
                     <SheetDescription className='sr-only'>Sheet Description</SheetDescription>
@@ -124,7 +124,7 @@ const MobileSheet = () => {
                     <ScrollBar orientation='vertical' />
                 </ScrollArea>
 
-                <SheetFooter className='border-t px-4 py-2'>
+                <SheetFooter className='border-t px-4 py-2 sm:flex-col-reverse sm:justify-start sm:space-x-0'>
                     {!isLogin ? (
                         <Button className='rounded-full' onClick={() => setIsLogin(true)}>
                             Masuk atau Daftar Akun
