fdab643973c029bb5d8dfc85fc358814a0b7f26b
Mario <mariomad2296@gmail.com>
2024-10-17 01:26:36 +0800

    Add Skeleton to Section News

--- src/features/general/home/section-news.tsx
+++ src/features/general/home/section-news.tsx
@@ -1,6 +1,47 @@
+import * as React from 'react';
+
 import Link from 'next/link';
 
+import { Skeleton } from '@/components/ui/skeleton';
 import { newsLists } from './news-lists';
+
+const NewsBlock = () => {
+    const [news, setNews] = React.useState<typeof newsLists>([]);
+
+    React.useEffect(() => {
+        setTimeout(() => setNews(newsLists), 5000);
+    }, []);
+
+    return (
+        <div className='flex flex-col gap-y-4'>
+            {news.length
+                ? news.map((item, i) => (
+                      <div key={`${i}${item.title}`} className='flex items-center gap-2'>
+                          <img
+                              className='aspect-square size-[5.438rem] rounded-2xl border object-cover object-center'
+                              width={100}
+                              height={100}
+                              src={item.image}
+                              alt={item.title}
+                          />
+                          <div className='flex flex-col gap-1.5'>
+                              <h3 className='text-base font-semibold text-primary'>{item.title}</h3>
+                              <p className='text-sm font-normal text-green-600'>{item.category}</p>
+                          </div>
+                      </div>
+                  ))
+                : Array.from({ length: 5 }).map((_, i) => (
+                      <div key={i} className='flex items-center gap-2'>
+                          <Skeleton className='aspect-square size-[5.438rem] rounded-2xl border object-cover object-center' />
+                          <div className='flex flex-col gap-2.5'>
+                              <Skeleton className='h-5 w-52 rounded-full' />
+                              <Skeleton className='h-2.5 w-24 rounded-full' />
+                          </div>
+                      </div>
+                  ))}
+        </div>
+    );
+};
 
 const SectionNews = () => {
     return (
@@ -11,23 +52,7 @@ const SectionNews = () => {
                     Lihat Semua
                 </Link>
             </div>
-            <div className='flex flex-col gap-y-4'>
-                {newsLists.map((item, i) => (
-                    <div key={`${i}${item.title}`} className='flex items-center gap-2'>
-                        <img
-                            className='aspect-square size-[5.438rem] rounded-2xl border object-cover object-center'
-                            width={100}
-                            height={100}
-                            src={item.image}
-                            alt={item.title}
-                        />
-                        <div className='flex flex-col gap-1.5'>
-                            <h3 className='text-base font-semibold text-primary'>{item.title}</h3>
-                            <p className='text-sm font-normal text-green-600'>{item.category}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
+            <NewsBlock />
         </section>
     );
 };
