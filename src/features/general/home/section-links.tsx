c4daa64a46fd190ac510ecc64273f8111d664e58
Mario <mariomad2296@gmail.com>
2024-10-16 23:52:09 +0800

    Update section-links.tsx

--- src/features/general/home/section-links.tsx
+++ src/features/general/home/section-links.tsx
@@ -1,7 +1,7 @@
 const SectionLinks = () => {
     return (
         <section id='links' className='py-10 text-center'>
-            <h2>// TODO : List of Links</h2>
+            <h2>TODO : List of Links</h2>
         </section>
     );
 };
