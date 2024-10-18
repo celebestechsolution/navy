6ef2d9b4fe29b1778b6aac1cf630fa266d6ba034
Mario <mariomad2296@gmail.com>
2024-10-18 20:59:25 +0800

    Finishing page Complaints V.1

--- src/pages/general/complaints/index.tsx
+++ src/pages/general/complaints/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPageWithLayout } from '@/types/app/next-layout';
 
+import { ComplaintsContent } from '@/features/general/complaints/complaints-content';
+
 import { GeneralLayout } from '@/layouts/general-layout';
 import { MobileNavigation } from '@/layouts/mobile-navigation';
 import { RootLayout } from '@/layouts/root-layout';
 
 const ComplaintsPage: NextPageWithLayout = () => {
-    return <div>ComplaintsPage</div>;
+    return <ComplaintsContent />;
 };
 
 ComplaintsPage.getLayout = function getLayout(page: React.ReactElement) {
