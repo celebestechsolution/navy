9b468ba412d587a86a1309ffafa3f12f71abfdc3
Mario <mariomad2296@gmail.com>
2024-10-18 11:06:18 +0800

    Finishing page Permits/Applicants V.1

--- src/features/general/permits/applicants/permit-applicants-content.tsx
+++ src/features/general/permits/applicants/permit-applicants-content.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 
+import { cn } from '@/lib/utils';
+import { applicants } from './permit-applicants-data';
+
 import { AppShell } from '@/components/shells/app-shell';
 import {
     Breadcrumb,
@@ -9,6 +12,7 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 const PermitApplicantsContent = () => {
     return (
@@ -39,6 +43,23 @@ const PermitApplicantsContent = () => {
             <AppShell className='border-t border-border'>
                 <section id='permit-applicants-content' className='space-y-6'>
                     <h1 className='text-lg font-bold text-primary'>Jumlah Pemohon Izin</h1>
+                    <div className='grid grid-cols-2 gap-2'>
+                        {applicants.map((item, i) => (
+                            <Card
+                                key={i + item.value}
+                                className={cn('space-y-2 p-4 shadow-none', i === 0 && 'col-span-full')}>
+                                <CardHeader className='p-0'>
+                                    <CardTitle className='text-sm font-medium text-primary'>{item.label}</CardTitle>
+                                </CardHeader>
+                                <CardContent className='p-0'>
+                                    <div className='text-2xl font-bold text-primary'>{item.value}</div>
+                                </CardContent>
+                                <CardFooter className='p-0'>
+                                    <p className='text-sm text-green-600'>Pemohon</p>
+                                </CardFooter>
+                            </Card>
+                        ))}
+                    </div>
                 </section>
             </AppShell>
         </>
---
+++ src/features/general/permits/applicants/permit-applicants-data.ts
@@ -0,0 +1,37 @@
+type Applicant = {
+    label: string;
+    value: number;
+};
+
+const applicants: Applicant[] = [
+    {
+        label: 'Pendaftaran Hari Ini',
+        value: 23,
+    },
+    {
+        label: 'Sementara Proses',
+        value: 51,
+    },
+    {
+        label: 'Izin TTE Hari ini',
+        value: 12,
+    },
+    {
+        label: 'Bulan Ini',
+        value: 55,
+    },
+    {
+        label: 'Bulan Lalu',
+        value: 45,
+    },
+    {
+        label: 'Tahun Ini 2024',
+        value: 102,
+    },
+    {
+        label: 'Tahun Lalu 2023',
+        value: 299,
+    },
+];
+
+export { applicants };
