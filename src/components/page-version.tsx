101cbf14cfc41b5b0de0d587a255aea2b3e6c1e5
Mario <mariomad2296@gmail.com>
2024-10-23 13:19:32 +0800

    Create component page-version.tsx

---
+++ src/components/page-version.tsx
@@ -0,0 +1,13 @@
+import { Badge } from './ui/badge';
+
+const PageVersion = ({ label = 2 }: { label: number | string }) => {
+    return (
+        <div className='absolute -top-3 right-2'>
+            <Badge variant='outline' className='bg-background'>
+                Version : {label.toString()}
+            </Badge>
+        </div>
+    );
+};
+
+export { PageVersion };
