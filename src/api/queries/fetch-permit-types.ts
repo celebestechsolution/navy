a215c59869aed2f925a8ca0f661d20ff9d5af6e5
Mario <mariomad2296@gmail.com>
2024-10-23 12:31:25 +0800

    Implement Filter functionality when fetching Permit Types

--- src/api/queries/fetch-permit-types.ts
+++ src/api/queries/fetch-permit-types.ts
@@ -3,17 +3,25 @@ import { useQuery } from '@tanstack/react-query';
 import type { ApiResponse } from '@/types/api';
 import type { PermitType } from '@/types/api/permit';
 
+import { useDebounce } from '@/hooks/use-debounce';
 import { axios } from '@/lib/axios';
+import { usePermitTypesFilters } from '@/services/states/use-permit-types-filters';
 
-async function fetchPermitTypes() {
-    const { data } = await axios.get<ApiResponse<PermitType[]>>('/permit/types');
+async function fetchPermitTypes(search: string) {
+    const { data } = await axios.get<ApiResponse<PermitType[]>>('/permit/types', {
+        params: { search },
+    });
 
     return data.data;
 }
 
 export const useFetchPermitTypes = () => {
+    const search = usePermitTypesFilters((state) => state.search);
+
+    const debounceValue = useDebounce(search);
+
     return useQuery<PermitType[]>({
-        queryKey: ['permit-types'],
-        queryFn: fetchPermitTypes,
+        queryKey: ['permit-types', { search: debounceValue }],
+        queryFn: () => fetchPermitTypes(debounceValue),
     });
 };
