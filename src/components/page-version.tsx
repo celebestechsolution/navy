7690961e3c3fa7cd59cd9b05b29bce6078f376c9
Mario <mariomad2296@gmail.com>
2024-10-23 13:21:10 +0800

    Update page-version.tsx

--- src/components/page-version.tsx
+++ src/components/page-version.tsx
@@ -1,6 +1,6 @@
 import { Badge } from './ui/badge';
 
-const PageVersion = ({ label = 2 }: { label: number | string }) => {
+const PageVersion = ({ label = 2 }: { label?: number | string }) => {
     return (
         <div className='absolute -top-3 right-2'>
             <Badge variant='outline' className='bg-background'>
