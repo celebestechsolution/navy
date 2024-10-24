7c1336ce0cce64c1ef35ff3f6bfdcb0521830cbd
Mario <mariomad2296@gmail.com>
2024-10-24 20:19:24 +0800

    Update section-hero.tsx

--- src/features/general/home/section-hero.tsx
+++ src/features/general/home/section-hero.tsx
@@ -1,10 +1,11 @@
 const SectionHero = () => {
     return (
         <section id='hero' className='space-y-4 py-10 text-center'>
-            <h1 className='text-[1.625rem]/[1.875rem] font-extrabold text-primary'>
+            {/* text-[1.625rem]/[1.875rem] */}
+            <h1 className='text-3xl font-extrabold text-primary'>
                 Sistem Informasi dan Aplikasi Perizinan Berbasis Online Single Submission
             </h1>
-            <p className='text-sm font-medium text-muted-foreground'>Simple Izinnya, Safe Investasinya</p>
+            <p className='text-base font-medium text-muted-foreground'>Simple Izinnya, Safe Investasinya</p>
         </section>
     );
 };
