19e6848905e6ce267b487c1515df9fbd80ea93ee
Mario <mariomad2296@gmail.com>
2024-10-24 13:13:26 +0800

    Update Mobile Sheet Links

--- src/layouts/mobile-sheet-links.ts
+++ src/layouts/mobile-sheet-links.ts
@@ -2,15 +2,24 @@ import { type UrlObject } from 'url';
 
 import type { TablerIconName } from '@/types/app/tabler-icon';
 
-interface Link {
+interface LinkUseImage {
+    use: 'image';
+    image: string;
+}
+
+interface LinkUseIcon {
+    use: 'icon';
+    icon: TablerIconName;
+}
+
+type DefaultLink = {
     label: string;
     href: UrlObject | string;
-    icon?: TablerIconName;
-}
+} & (LinkUseIcon | LinkUseImage);
 
 interface Menu {
     label: string;
-    links: Link[];
+    links: DefaultLink[];
 }
 
 const menus: Menu[] = [
@@ -20,10 +29,14 @@ const menus: Menu[] = [
             {
                 label: 'Beranda',
                 href: '/',
+                use: 'icon',
+                icon: 'IconHome',
             },
             {
                 label: 'Berita Terkini',
                 href: '/latest-news',
+                use: 'icon',
+                icon: 'IconNews',
             },
         ],
     },
@@ -33,21 +46,25 @@ const menus: Menu[] = [
             {
                 label: 'Daftar Izin Proses',
                 href: '/permits/lists',
+                use: 'icon',
                 icon: 'IconClipboardList',
             },
             {
                 label: 'Daftar Jenis Izin',
                 href: '/permits/types',
+                use: 'icon',
                 icon: 'IconFileDigit',
             },
             {
                 label: 'Jumlah Pemohon Izin',
                 href: '/permits/applicants',
+                use: 'icon',
                 icon: 'IconFileDiff',
             },
             {
                 label: 'Standar Pelayanan Perizinan',
                 href: '/permits/service-standards',
+                use: 'icon',
                 icon: 'IconFileLambda',
             },
         ],
@@ -58,18 +75,26 @@ const menus: Menu[] = [
             {
                 label: 'SIAP BOSS',
                 href: '/innovations/siap-boss',
+                use: 'image',
+                image: '/images/siap-boss-logo.png',
             },
             {
                 label: 'Rajin',
                 href: '/innovations/rajin',
+                use: 'image',
+                image: '/images/rajin-logo.png',
             },
             {
                 label: 'Paksi',
                 href: '/innovations/paksi',
+                use: 'image',
+                image: '/images/paksi-logo.png',
             },
             {
                 label: 'Jabat Erat',
                 href: '/innovations/jabat-erat',
+                use: 'image',
+                image: '/images/jabat-erat-logo.png',
             },
         ],
     },
@@ -79,21 +104,25 @@ const menus: Menu[] = [
             {
                 label: 'Daftar Pengaduan',
                 href: '/complaints/lists',
+                use: 'icon',
                 icon: 'IconClipboardText',
             },
             {
                 label: 'Kategori Pengaduan & Dasar Hukum',
                 href: '/complaints/categories',
+                use: 'icon',
                 icon: 'IconFileText',
             },
             {
                 label: 'Buat Pengaduan Baru',
                 href: '/complaints/create',
+                use: 'icon',
                 icon: 'IconClipboardPlus',
             },
             {
                 label: 'Pengaduan Saya',
                 href: '/dashboard',
+                use: 'icon',
                 icon: 'IconFileReport',
             },
         ],
@@ -104,20 +133,23 @@ const menus: Menu[] = [
             {
                 label: 'Profil DPMPTSP',
                 href: '/about/profile',
+                use: 'icon',
                 icon: 'IconBuildingBank',
             },
             {
                 label: 'Tugas Pokok & Fungsi',
                 href: '/about/duties',
+                use: 'icon',
                 icon: 'IconBooks',
             },
             {
                 label: 'Penghargaan',
                 href: '/about/awards',
+                use: 'icon',
                 icon: 'IconFileCertificate',
             },
         ],
     },
 ];
 
-export { menus, type Link, type Menu };
+export { menus, type DefaultLink, type Menu };
